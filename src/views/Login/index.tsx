8e15d5e1088d29c791474be7957f00323db4850d
acampillo <acampillo@edesarrollos.com>
2022-09-21 20:47:03 -0700

    sign in google y facebook, falta creación desde mongo

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -5,6 +5,7 @@ import logo from '../../assets/logo-example.jpg';
 import logoBg from '../../assets/delivery-bg.jpg';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
+import { signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import '../../assets/styles/Login.css'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 
@@ -31,10 +32,34 @@ const Login = () => {
     }
   }
 
+  const signInGoogle = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/userinfo.email') // permiso correo
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user;
+      console.log(user);      
+    } catch (e) {
+      console.log(e);
+      message.error("Error, al iniciar con Google.");
+    }
+  }
+
+  const signInFacebook = async () => {
+    try {
+      const provider = new FacebookAuthProvider();
+      provider.addScope('email') // permiso correo
+      await signInWithPopup(auth, provider);
+    } catch (e) {
+      console.log(e);
+      message.error("Error, al iniciar con Facebook.");
+    }
+  }
+
   return (
     <div className="app-container">
       <Row justify="center">
-        <Col >
+        <Col className='main-col'>
           <div className='app-login-wrapper'>
             <Card
               className='app-login-card'
@@ -71,6 +96,7 @@ const Login = () => {
                   name="email"
                   rules={[{ required: true, message: 'Favor de escribir el correo.' }]}
                   hasFeedback
+                  style={{ marginBottom: '10px' }}
                 >
                   <Input
                     prefix={<UserOutlined className="site-form-item-icon" />}
@@ -82,6 +108,8 @@ const Login = () => {
                 <Form.Item
                   name="password"
                   rules={[{ required: true, message: 'Favor de escribir la contraseña.' }]}
+                  hasFeedback
+                  style={{ marginBottom: '10px' }}
                 >
                   <Input.Password
                     prefix={<LockOutlined className="site-form-item-icon" />}
@@ -90,21 +118,63 @@ const Login = () => {
                     placeholder="Contraseña"
                   />
                 </Form.Item>
-                <Form.Item>
-                  <Button type="primary" htmlType="submit" loading={loading}>
+                <Form.Item style={{ marginBottom: '10px' }}>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    loading={loading}
+                    block
+                  >
                     Entrar
                   </Button>
                 </Form.Item>
+                <Divider orientation='center' className='divider-or'> ó </Divider>
+                {/* icons google - facebook */}
+                <Row gutter={[16, 0]} justify="space-evenly">
+                  <Col span={12}>
+                    <Button
+                      type='link'
+                      style={{
+                        padding: '0 10px',
+                        borderRadius: '50px',
+                      }}
+                      onClick={signInGoogle}
+                    >
+                      <Avatar
+                        src='/google.png'
+                        size="large"
+                        className='icon-google'
+                      />
+                    </Button>
+                  </Col>
+                  <Col span={12}>
+                    <Button
+                      type='link'
+                      style={{
+                        padding: '0 10px',
+                        borderRadius: '50px',
+                      }}
+                      onClick={signInFacebook}
+                    >
+                      <Avatar
+                        src='/facebook.png'
+                        size="large"
+                        className='icon-facebook'
+                      />
+                    </Button>
+                  </Col>
+                </Row>
               </Form>
-              <Row gutter={[16, 0]} justify="space-around">
+              <Divider />
+              <Row gutter={[16, 0]} justify="space-evenly">
                 <Col span={12}>
                   <a href="#app-store">
-                    <img src="/app-store.png" alt="img-app-store" width="120" />
+                    <img src="/app-store.png" alt="img-app-store" width="130" />
                   </a>
                 </Col>
                 <Col span={12}>
                   <a href="#google-play">
-                    <img src="/google-play.png" alt="img-google-play" width="120" />
+                    <img src="/google-play.png" alt="img-google-play" width="130" />
                   </a>
                 </Col>
               </Row>
